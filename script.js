b4f9c0e039da3a9eab725bdb5da02996211b7ab2
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2021-02-21 03:36:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ const locations = {
 main()
 
 const Main = document.querySelector('main')
+const Canvas = document.createElement('canvas')
+Canvas.ctx = Canvas.getContext('2d')
+
 async function main() {
     if (window.location.pathname)
         show_article()
@@ -52,6 +55,9 @@ async function main() {
             history.pushState({}, '', '/')
         }
     })
+    
+    Canvas.width = Canvas.height = 4
+    Canvas.ctx.filter = 'saturate(1000%)'
 }
 async function show_article() {
     const [location_index, category_index, ...id_array] = atob(window.location.pathname.split('/')[2])
@@ -102,6 +108,7 @@ function article_from_remote(id, location, category, remote) {
     })
     return article
 }
+
 async function db(path) {
     const response = await fetch(`https://arcadia-high-mobile.firebaseio.com/${path}.json`)
     const data = await response.json()
@@ -137,14 +144,14 @@ function make_snippet(article) {
         ].join(''))
     Snippet.classList.toggle('featured', article.featured)
 
-    for (const name of['image', 'blur']) {
-        const element = Snippet.querySelector('.' + name)
-        if (article.images)
-            element.src = article.images[0]
-        else
-            element.hidden = true
+    const Image = Snippet.querySelector('.image')
+    if(article.images){
+        Image.src = article.images[0]
+        gradient_background(Snippet,Image)
+    }else{
+        Image.remove()
     }
-
+    
     for (const attribute of['title'])
         Snippet.querySelector('.' + attribute).innerHTML = article[attribute]
 
@@ -154,6 +161,33 @@ function make_snippet(article) {
         event.preventDefault()
     })
     return Snippet
+}
+
+function gradient_background(element,image){
+  image.crossOrigin = 'Anonymous';
+  image.addEventListener('load',()=>{
+    Canvas.ctx.drawImage(image,0,0,Canvas.width,Canvas.height)
+    const data = Canvas.ctx.getImageData(0, 0, Canvas.width, Canvas.height).data
+    let colors = []
+    for(let i = 0; i < Canvas.width*Canvas.height*4; i+=4){
+      colors.push(`rgba(${data[i+0]}, ${data[i+1]}, ${data[i+2]}, 0.1)`)
+    }
+    let gradients = Array(5).fill(0).map(
+      _ => `radial-gradient(
+        ${rand_corner()},
+        ${rand_value(colors)},${rand_value(colors)},
+        transparent)`
+    )
+    gradients.unshift(`radial-gradient(${rand_corner()},transparent,white)`)
+    element.style.backgroundImage = gradients.join(',')
+  })
+}
+
+function rand_value(array){
+  return array[Math.floor(Math.random()*array.length)]
+}
+function rand_corner(){
+  return 'circle at ' + rand_value(['top left','top right','bottom left','bottom right'])
 }
 
 function slugify(text) {
